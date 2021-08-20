875e067c276d5db5dfa8ec12f76f6a0f1b1da57f
piotr-galas <galaspiotrek@gmail.com>
2021-08-20 11:32:26 +0200

    Add search fileds to filters

--- app/javascript/packs/filters/filter_form.js
+++ app/javascript/packs/filters/filter_form.js
@@ -2,6 +2,7 @@ class ToolbarForm {
   constructor(formNode) {
     this.formNode = formNode;
     this.downloadButtonNode = formNode.querySelector('.download-button')
+    this.searchInputs = formNode.querySelectorAll('input')
     this.setInitialValues();
     this.addListeners();
   }
@@ -12,6 +13,11 @@ class ToolbarForm {
       selectNode.addEventListener('change', event => this.submitForm('Filter'));
     })
     this.downloadButtonNode.addEventListener('click', event => this.submitForm('Export'))
+
+    this.searchInputs.forEach((inputNode) => {
+      inputNode.addEventListener('change', event => this.submitForm('Filter'))
+    });
+
   }
 
   setInitialValues(){
